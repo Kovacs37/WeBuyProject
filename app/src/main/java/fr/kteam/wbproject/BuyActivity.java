965b52bc8f2cca8320e7810b22d18bf6260b4a8d
Kovacs <mrkovacs28@gmail.com>
2020-03-01 18:05:15 +0100

    Initial commit

---
+++ app/src/main/java/fr/kteam/wbproject/BuyActivity.java
@@ -0,0 +1,14 @@
+package fr.kteam.wbproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+}
---
+++ app/src/main/java/fr/kteam/wbproject/BuyDetailedActivity.java
@@ -0,0 +1,14 @@
+package fr.kteam.wbproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyDetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_detailed);
+    }
+}
